ee8e38e4aa00555870cdf5d292cc492602cc65ed
CaritolaDiosadelOlimpo <carolina.medina@usach.cl>
2020-04-22 00:39:52 -0400

    installing plugin booklet

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#book').booklet();
+});
+
+/* $(function(){
+    $('#book').booklet({
+        width:700,
+        height:500,
+        closed:1,
+    });
+}); */
