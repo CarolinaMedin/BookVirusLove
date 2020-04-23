c82755e01fe986ac4f4f9ce15f4933bd0cce033e
CaritolaDiosadelOlimpo <carolina.medina@usach.cl>
2020-04-22 22:33:33 -0400

    create of styles

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 $(document).ready(function(){
-    $('#book').booklet();
-});
-
-/* $(function(){
     $('#book').booklet({
-        width:700,
-        height:500,
+        width:900,
+        height:600,
         closed:1,
     });
-}); */
+});
+
