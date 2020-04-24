3cb7121f515516e8a1e265ec8ab8ae71d1d161ee
CaritolaDiosadelOlimpo <carolina.medina@usach.cl>
2020-04-23 21:00:34 -0400

    create search and add style

--- js/main.js
+++ js/main.js
@@ -3,6 +3,20 @@ $(document).ready(function(){
         width:900,
         height:600,
         closed:1,
+        covers:1,
+        pagePadding:0,
+    });
+
+    $('#previous').click(function(){
+        $('#book').booklet('prev');
+    });
+
+    $('#next').click(function(){
+        $('#book').booklet('next');
+    });
+    
+    $('#btnSearch').click(function(){
+        $('#book').booklet('gotopage', $('#searchField').val());
     });
 });
 
